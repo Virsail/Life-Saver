45888d27302bf5ef74952362506e29cede86f3c9
virsail <virsaileric@gmail.com>
2020-09-20 17:05:32 +0300

    import session ,db

---
+++ app/auth/views.py
@@ -0,0 +1,6 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+from . import auth
+from flask_login import login_user, logout_user, login_required
+from .. import db
