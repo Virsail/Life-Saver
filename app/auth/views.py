dd0626870cd3762204ba7f15dce3c550524e2389
virsail <virsaileric@gmail.com>
2020-09-20 17:18:54 +0300

    add og out function for user

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,6 +18,26 @@ def login():
 
     title = "pitch login"
     return render_template('auth/login.html', login_form=login_form, title=title)
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
+
+
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    title = "New Account"
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', registration_form=form, title=title)
     
 
 
