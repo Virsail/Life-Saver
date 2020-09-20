7936bb60b2a69a813de370f55c2ba4378ec5fd30
virsail <virsaileric@gmail.com>
2020-09-20 16:50:07 +0300

    add registration class to init.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,3 +3,5 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
 from wtforms import ValidationError
+
+class RegistrationForm(FlaskForm):
