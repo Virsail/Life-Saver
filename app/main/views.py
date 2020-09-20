58ee67cdccb4615fe6c2b4b3ad697a6b7bae88d2
virsail <virsaileric@gmail.com>
2020-09-20 17:54:44 +0300

    add function that returns user and pitch data

--- app/main/views.py
+++ app/main/views.py
@@ -97,4 +97,46 @@ def update_profile(uname):
 
         return redirect(url_for('.profile',uname=user.username))
 
+      return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods=['POST'])
+@login_required
+def update_pic(uname):
+    '''
+    View update pic profile function that returns the uppdate profile pic page
+    '''
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
+
+
+
+@main.route('/home', methods = ['GET', 'POST'])
+@login_required
+def index2():
+    '''
+    View index2 function that returns the home page
+    '''
+    advertisement = Pitch.query.filter_by(category="advertisement").order_by(Pitch.date.desc()).all()
+    project = Pitch.query.filter_by(category="project").order_by(Pitch.date.desc()).all()
+    general = Pitch.query.filter_by(category="general").order_by(Pitch.date.desc()).all()
+    sale = Pitch.query.filter_by(category="sale").order_by(Pitch.date.desc()).all()
+    pitch = Pitch.get_all_pitches()
+
+    title = 'Home | One Min Pitch'
+    return render_template('home.html', title = title, pitch = pitch, advertisement = advertisement, project = project, general = general, sale = sale)
+
+
+@main.route('/pitch/new',methods = ['GET','POST'])
+@login_required
+def pitch():
+    '''
+    View pitch function that returns the pitch page and data
+    '''
+
   
